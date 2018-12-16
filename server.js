ea0de249e5dd51623ea1e377f96febd2c2054af6
Nikki1686 <nikki.l.carthen@gmail.com>
2018-12-15 17:31:19 -0800

    added the base files

---
+++ server.js
@@ -0,0 +1,229 @@
+'use strict';
+
+
+//Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
+
+//Load env vars;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+
+//postgres
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+//app
+const app = express();
+app.use(cors());
+
+//routes
+app.get('/location', getLocation);
+app.get('/weather', getWeather);
+app.get('/yelp', getYelp);
+app.get('/movies', getMov);
+
+//========================LOOK FOR RESULTS IN DATABASE==============================================//
+
+function lookup(options) {
+  const SQL = `SELECT * FROM ${options.tableName} WHERE location_id=$1;`;
+  const values = [options.location];
+
+  client.query(SQL, values)
+    .then(result => {
+      if (result.rowCount > 0) {
+        options.cacheHit(result);
+      } else{
+        options.cacheMiss();
+      }
+    })
+    .catch(error => handleError(error));
+}
+
+//Handlers
+function getLocation(request, response) {
+
+
+  Location.lookupLocation({
+    tableName: Location.tableName,
+
+    query: request.query.data,
+
+    cacheHit: function(result) {
+      response.send(result.rows[0]);
+    },
+
+    cacheMiss: function() {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.query}&key=${process.env.GEOCODE_API_KEY}`;
+      return superagent.get(url)
+        .then(result => {
+          const location = new Location(this.query, result);
+          location.save()
+            .then(location => response.send(location));
+            console.log(location, 'locationnnnnnnnnn');
+          })
+          .catch(error => handleError(error));
+        }
+      });
+}
+
+function getWeather(request, response) {
+
+  console.log('inside get weather');
+  const weatherHandler = {
+    tableName: Weather.tableName,
+
+    location: request.query.data.id,
+
+    cacheHit: function (result) {
+      response.send(result.rows);
+    },
+
+    cacheMiss: function () {
+      const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+      console.log(url);
+      superagent.get(url)
+        .then(result => {
+          const weatherSummaries = result.body.daily.data.map(day => {
+            const summary = new Weather(day);
+            summary.save(request.query.data.id);
+            return summary;
+          });
+          console.log(weatherSummaries, 'BOOOOOOOYYYYAAAAA');
+          response.send(weatherSummaries);
+        })
+        .catch(error => handleError(error, response));
+    }
+  };
+  
+  Weather.lookup(weatherHandler);
+}
+
+function getYelp (request, response){
+  return searchYelps(request.query.data)
+    .then(yelpData => {
+      response.send(yelpData);}
+    );
+}
+
+function getMov (request, response) {
+  return searchMovs(request.query.data)
+    .then(movData => {
+      response.send(movData);}
+    );
+}
+
+//Constructor Function
+function Location(query, res){
+  this.tableName = 'locations';
+  this.search_query = query;
+  this.formatted_query = res.body.results[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
+}
+
+function Weather(forecast) {
+  this.tableName = 'weathers';
+  this.forecast = forecast.summary;
+  this.time = new Date(forecast.time * 1000).toString().slice(0, 15);
+}
+
+Weather.tableName = 'weathers';
+Weather.lookup = lookup;
+
+function Bsns (bsns) {
+  this.name = bsns.name;
+  this.image_url = bsns.image_url;
+  this.price = bsns.price;
+  this.ratin5g = bsns.rating;
+  this.url = bsns.url;
+}
+
+function Movie(movie) {
+  this.title = movie.title;
+  this.overview = movie.overview;
+  this.average_votes = movie.vote_average;
+  this.total_votes = movie.vote_count;
+  this.image_url = `http://image.tmdb.org/t/p/w200_and_h300_bestv2${movie.poster_path}`;
+  this.popularity = movie.popularity;
+  this.released_on = movie.release_date;
+}
+
+//SQL
+Location.lookupLocation = (location) => {
+  const SQL = 'SELECT * FROM locations WHERE search_query=$1;';
+  const values = [location.query];
+
+  return client.query(SQL, values)
+    .then(result => {
+      if(result.rowCount > 0) {
+        location.cacheHit(result);
+      } else {
+        location.cacheMiss();
+      }
+    })
+    .catch(console.error);
+}
+
+//Search Functions
+Location.prototype = {
+  save: function() {
+    const SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;';
+    const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+
+    return client.query(SQL, values)
+      .then(result => {
+        this.id = result.rows[0].id;
+        return this;
+      });
+  }
+};
+
+Weather.prototype = {
+  save: function(location_id) {
+    const SQL = `INSERT INTO ${this.tableName} (forecast, time, location_id) VALUES ($1, $2, $3);`;
+    const values = [this.forecast, this.time, location_id];
+
+    client.query(SQL, values);
+  }
+};
+
+
+function searchMovs(query) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`;
+  return superagent.get(url)
+    .then(moviesData => {
+      // console.log(query);
+      return moviesData.body.results.map(movie => new Movie(movie));
+    });
+}
+
+function searchYelps(query) {
+  const url = `https://api.yelp.com/v3/businesses/search?term=delis&latitude=${query.latitude}&longitude=${query.longitude}`;
+  return superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(yelpData => {
+      // console.log(yelpData.body.businesses);
+      return yelpData.body.businesses.map(bsns => new Bsns(bsns));
+    })
+    .catch(err => console.error(err));
+}
+
+// Error messages
+app.get('/*', function(request, response) {
+  response.status(404).send('halp, you are in the wrong place');
+});
+
+// Error handler
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+app.listen(PORT, () => {
+  console.log(`app is up on port : ${PORT}`);
+});
